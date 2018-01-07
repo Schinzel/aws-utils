da2870c374b2f0f9c28ee8b6976b6ed3e4057088
Henrik Schinzel <henrik@schinzel.io>
2018-01-07 19:49:36 +0100

    Doc

--- src/main/java/io/schinzel/basicutils/s3handler/BucketCache.java
+++ src/main/java/io/schinzel/basicutils/s3handler/BucketCache.java
@@ -6,18 +6,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Purpose of this class is to check if a bucket exists, and cached the buckets that did exist.
+ * Purpose of this class is to check if a bucket exists, and cache the buckets that did exist.
+ * <p>
+ * The purpose of this is so that a clear error message can be thrown if a bucket does not exist.
+ * The reason that the existing buckets are cached is that the checking if a bucket exists takes
+ * surprisingly long time, much longer than an file upload.
+ * <p>
+ * Below is a performance measurement done 2018-01-07 on an approximately 200 Mbit/s connection
+ * which shows how slow the bucket checking operation is. The measurement is done when nothing has
+ * been cached.
+ * <p>
+ * Name:root Tot:2,433ms Avg:2,433.82ms Hits:1
+ * sublaps
+ * ┗━ Name:CreateObject Root:73% Parent:73% Tot:1,765ms Avg:1,765.14ms Hits:1
+ * --┗━ sublaps
+ * ----┗━ Name:GetTransferManager Root:36% Parent:49% Tot:865ms Avg:865.15ms Hits:1
+ * ----┗━ Name:CheckIfBucketExists Root:37% Parent:51% Tot:896ms Avg:896.29ms Hits:1
+ * ┗━ Name:Upload Root:7% Parent:7% Tot:163ms Avg:163.74ms Hits:1
+ * ┗━ Name:Download Root:10% Parent:10% Tot:241ms Avg:241.40ms Hits:1
+ * ┗━ Name:Delete Root:5% Parent:5% Tot:123ms Avg:123.89ms Hits:1
  * <p>
  * Created by Schinzel on 2018-01-03
  */
 class BucketCache {
     /**
-     * A cache for existing buckets on S3. Is necessary as the doesBucketExist
-     * method takes a surprisingly long time (Schinzel 2016).
+     * Cache for existing buckets on S3.
      */
     private static final List<String> EXISTING_BUCKETS_CACHE = new ArrayList<>();
 
 
+    /**
+     * @param transferManager An Amazon transfer manager
+     * @param bucketName      The name of a bucket
+     * @return True if the argument bucket exists, else false
+     */
     static boolean doesBucketExist(TransferManager transferManager, String bucketName) {
         //If buckets cache contains the argument bucket name
         if (EXISTING_BUCKETS_CACHE.contains(bucketName)) {
